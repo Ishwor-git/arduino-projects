370d1c3daa6a4d9ab0f8023428e258de90940ffb
hydra <raishwor2@gmail.com>
2023-06-22 23:03:19 +0545

    dc motor

--- ir_sensor/ir_sensor.ino
+++ ir_sensor/ir_sensor.ino
@@ -8,12 +8,13 @@ void setup() {
 
 void loop() {
   int irData = analogRead(irPin);
-  //checking sensor output value value
+/*
   Serial.println("");
   Serial.print("IR Sensor ");
   Serial.print(" Value = ");
   Serial.print(irData);
 //  delay(1000);
+*/
 
   if (irData <= 200){
     digitalWrite(buzzPin,HIGH);
