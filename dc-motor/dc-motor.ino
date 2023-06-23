0002254a38e45cfee571cabff92a2a6861def545
hydra <raishwor2@gmail.com>
2023-06-23 07:37:37 +0545

    dc motor rotation

--- dc-motor/dc-motor.ino
+++ dc-motor/dc-motor.ino
@@ -1,15 +1,25 @@
-const int inPin1 = 4, inPin2 = 4, enPin =3;
+const int inPin1 = 4, inPin2 = 5, enPin =3;
 
+void rotate_clockwise(){
+  digitalWrite(inPin1,HIGH);
+  digitalWrite(inPin2,LOW);
+}
+
+void rotate_anticlockwise(){
+  digitalWrite(inPin1,LOW);
+  digitalWrite(inPin2,HIGH);
+}
 
 
 void setup(){
   Serial.begin(9600);
-  pinMode(13,OUTPUT);
+  pinMode(inPin1,OUTPUT);
+  pinMode(inPin2,OUTPUT);
 }
 
 
 
 void loop(){
-
+  rotate_anticlockwise();
 }
 
